ea51e9429642e60ff38e1cdfd8dff4030d1f5549
Lexa <lexa2987@mail.ru>
2018-09-06 19:38:42 +0300

    fix js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.css';
 import './media.css';
+import im from './img/bg_blog.png'
 
 const photo = document.querySelectorAll('.wrap-photo .photo');
 photo.forEach(item => {
@@ -32,13 +33,34 @@ closeBtn.addEventListener('click', () => {
    closeNav();
 });
 
+const bgItem = {
+    init: function () {
+        const blogItem = document.querySelectorAll('.blog-item');
+        blogItem.forEach(item => {
+            item.addEventListener('mouseover', () => {
+                item.style.color = '#fff';
+                item.style.background = `url(${im}) no-repeat`;
+            });
+
+            item.addEventListener('mouseleave', ()=> {
+                item.style.color = '#000';
+                item.style.background = 'transparent';
+            })
+        })
+    }
+};
+
+bgItem.init();
+
+
 const slid = {
   init: function () {
       const slickNext = document.querySelector('.slick-next');
       const slickPrev = document.querySelector('.slick-prev');
+      const photoWidth = document.querySelectorAll('.wrap-photo .photo')[0].clientWidth;
       slickPrev.addEventListener('click', () => {
           const posX = document.querySelectorAll('.slick-track')[0].style.transform;
-          if (Number(String(posX).slice(12, -13)) !== -1200) {
+          if ((document.querySelectorAll('.wrap-photo')[(document.querySelectorAll('.wrap-photo').length -1)].getClientRects()[0].x - Number(photoWidth)) > document.querySelectorAll('.slick-list')[0].clientWidth) {
               document.querySelectorAll('.slick-track')[0].style.transform = `translate3d(${(Number(String(posX).slice(12, -13)) - 200)}px, 0px, 0px)`;
           }
       });
